a37c2e5d63716cf7b09a8010c50bd94586e674de
Tim Hawes <me@timhawes.com>
2023-11-18 14:34:11 +0000

    Add packet reply timeouts

--- settings.py
+++ settings.py
@@ -45,6 +45,8 @@ class defaults:
     SYNC_FILES = True
     TOOLSTATE_FILE = None
     SYNC_CHUNK_SIZE = 256
+    SYNC_REPLY_TIMEOUT = 20
+    PACKET_REPLY_TIMEOUT = 5
     TIME_SEND_INTERVAL = 3600
     PING_INTERVAL = 30
     PONG_RECEIVE_TIMEOUT = 75
@@ -113,6 +115,12 @@ SYNC_FIRMWARE = getenv("SYNC_FIRMWARE", defaults.SYNC_FIRMWARE, parser=parse_boo
 SYNC_FILES = getenv("SYNC_FILES", defaults.SYNC_FILES, parser=parse_boolean)
 TOOLSTATE_FILE = getenv("TOOLSTATE_FILE", defaults.TOOLSTATE_FILE)
 SYNC_CHUNK_SIZE = getenv("SYNC_CHUNK_SIZE", defaults.SYNC_CHUNK_SIZE, parser=int)
+SYNC_REPLY_TIMEOUT = getenv(
+    "SYNC_REPLY_TIMEOUT", defaults.SYNC_REPLY_TIMEOUT, parser=int
+)
+PACKET_REPLY_TIMEOUT = getenv(
+    "PACKET_REPLY_TIMEOUT", defaults.PACKET_REPLY_TIMEOUT, parser=int
+)
 TIME_SEND_INTERVAL = getenv(
     "TIME_SEND_INTERVAL", defaults.TIME_SEND_INTERVAL, parser=int
 )
